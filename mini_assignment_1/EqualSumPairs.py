cbe49cc924c5f9a5df4acd58ccc7ed5fd9067a9d
niharparmar-deloitte <pyogeshbhai@deloitte.com>
2022-04-05 13:06:03 +0530

    changes-done-for-output

--- mini_assignment_1/EqualSumPairs.py
+++ mini_assignment_1/EqualSumPairs.py
@@ -2,18 +2,3 @@ from mini_assignment_1.StringClass import StringClass
 from mini_assignment_1.PairsPossible import PairsPossible
 from mini_assignment_1.SearchCommonElements import SearchCommonElements
 
-
-
-
-
-
-userinput=input()
-objstringclass=StringClass(userinput)
-print("Length of string is: ",objstringclass.printlength())
-print("String converted to list: ",objstringclass.converttolist())
-objpairspossible=PairsPossible(userinput)
-print("All possible pairs are: ")
-objpairspossible.pair()
-objsearchcommonelements=SearchCommonElements(userinput)
-print()
-print("Common elements are: ",objsearchcommonelements.commonele())
