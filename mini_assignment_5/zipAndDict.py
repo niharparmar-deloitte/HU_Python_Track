5c96dc5bd11b12cc7da4df9e559b0b38d9c9474f
niharparmar-deloitte <pyogeshbhai@deloitte.com>
2022-04-06 12:27:11 +0530

    key-pair-values

---
+++ mini_assignment_5/zipAndDict.py
@@ -0,0 +1,5 @@
+print("Implementing zip function: ")
+keys = ["Netflix", "Hulu", "Sling", "Hbo"]
+values = [198, 166, 237, 125]
+output_dictionary = dict(zip(keys, values))
+print(output_dictionary)
