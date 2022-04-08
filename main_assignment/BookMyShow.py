20b617445f462dcd0d78ab754dda19738faeb3c6
niharparmar-deloitte <pyogeshbhai@deloitte.com>
2022-04-08 12:50:08 +0530

    changes-done-and-added-few-features

--- main_assignment/BookMyShow.py
+++ main_assignment/BookMyShow.py
@@ -1,12 +1,19 @@
 from main_assignment.Admin import adminLogin
+from main_assignment.MoviesCRUD import moviescrud
+from main_assignment.MoviesOperation import moviesoperation
+from main_assignment.login import login
 from main_assignment.registration import register
 
+
+print("-----Welcome To BookMyShow-----")
 obj_register = register()
-print("******Welcome To BookMyShow******* ")
 obj_Admin = adminLogin()
+obj_MoviesCRUD = moviescrud()
+obj_MoviesOperation = moviesoperation()
+obj_login = login()
 
 while True:
-    print("Press 1 to login as admin: ")
+    print("Press 1 for admin: ")
     print("Press 2 to register: ")
     print("Press 3 to login as user")
     print("Press 4 to exit")
@@ -14,10 +21,10 @@ while True:
     if user_input == "4":
         break
     elif user_input == "1":
-        if obj_Admin.login_admin():
+        if obj_Admin.LoginAdmin():
             print("Login Successful! ")
             print()
-            print("******Welcome Admin******* ")
+            print("-----Welcome Admin-----")
             while True:
                 print("Press 1 to add movie info. ")
                 print("Press 2 to Edit movie info. ")
@@ -27,41 +34,44 @@ while True:
                 if admin_input == "4":
                     break
                 elif admin_input == "1":
-                    obj_Admin.addMovie()
+                    if obj_MoviesCRUD.AddMovie():
+                        pass
+                    else:
+                        continue
                 elif admin_input == "2":
-                    obj_Admin.EditMovie()
+                    obj_MoviesCRUD.EditMovie()
                 else:
-                    obj_Admin.deleteMovie()
+                    obj_MoviesCRUD.DeleteMovie()
         else:
-            print("Invalid Credentials")
+            print("Invalid Credentials. Please Try Again.")
     elif user_input == "2":
-        if obj_register.userregister():
+        if obj_register.UserRegister():
             print()
         else:
             continue
     elif user_input == "3":
-        if obj_register.userlogin():
+        if obj_login.UserLogin():
             print()
             while True:
-                obj_register.movies()
+                obj_MoviesOperation.Movies()
                 print("Enter Movie ID")
                 print("Enter 0 to logout")
-                choice=input()
-                if choice=="0":
+                choice = input()
+                if choice == "0":
                     break
                 else:
-                    obj_register.movieInfo(int(choice))
+                    obj_MoviesOperation.MovieInfo(int(choice))
                     print()
                     print("Press 1 to Book Tickets")
                     print("Press 2 to Cancel Tickets")
                     print("Press 3 for user ratings")
-                    userChoice=input()
-                    if userChoice=="2":
-                        obj_register.cancelTickets(int(choice))
-                    elif userChoice=="3":
-                        obj_register.Userratings(int(choice))
-                    elif userChoice=="1":
-                        obj_register.Booktickets(int(choice))
+                    userChoice = input()
+                    if userChoice == "2":
+                        obj_MoviesOperation.CancelTickets(int(choice))
+                    elif userChoice == "3":
+                        obj_MoviesOperation.UserRatings(int(choice))
+                    elif userChoice == "1":
+                        obj_MoviesOperation.BookTickets(int(choice))
                         print()
                     else:
                         print("Enter valid input!")
